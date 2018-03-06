b3d4463ba771ba4c904a7b40691c98bd626c93eb
Freek De Sagher (lin) <freek.de.sagher21@gmail.com>
2018-03-06 16:53:05 +0100

    Changed DesignByContract.h  - It now works both for windows and for Linux users

--- DesignByContract.h
+++ DesignByContract.h
@@ -4,11 +4,19 @@
 
 #include <cassert>
 
+#if defined(__unix__)
+    #define REQUIRE(assertion, what) \
+        if (!(assertion)) __assert (what, __FILE__, __LINE__);
+
+    #define ENSURE(assertion, what) \
+        if (!(assertion)) __assert (what, __FILE__, __LINE__);
+
+#elif defined(_WIN32) || defined(__WIN64)
 #define REQUIRE(assertion, what) \
-	if (!(assertion)) __assert (what, __FILE__, __LINE__);
+        if (!(assertion)) _assert (what, __FILE__, __LINE__);
 
-#define ENSURE(assertion, what) \
-	if (!(assertion)) __assert (what, __FILE__, __LINE__);
+    #define ENSURE(assertion, what) \
+        if (!(assertion)) _assert (what, __FILE__, __LINE__);
 
-
+#endif
 #endif //PROJECTSE_DESIGNBYCONTRACT_H
